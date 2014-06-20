6afbaec0a32b7513b2003d68ae69c00269bd55a7
Devin Hunt <devinhunt@gmail.com>
2014-06-20 12:38:47 +0100

    Initial Commit

---
+++ colordot-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  colordot-ios
+//
+//  Created by Devin Hunt on 6/20/14.
+//  Copyright (c) 2014 Devin Hunt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ colordot-iosTests/colordot_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  colordot_iosTests.m
+//  colordot-iosTests
+//
+//  Created by Devin Hunt on 6/20/14.
+//  Copyright (c) 2014 Devin Hunt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface colordot_iosTests : XCTestCase
+
+@end
+
+@implementation colordot_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
