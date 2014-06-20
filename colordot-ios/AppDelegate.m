f22a5a2d03c2423d40d37b736972877e2bd1586a
Devin Hunt <devinhunt@gmail.com>
2014-06-20 12:51:25 +0100

    Still got it. Simple nav controller setup to start rebuilding the palette interface.

--- colordot-ios/AppDelegate.m
+++ colordot-ios/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PaletteViewController.h"
 
 @implementation AppDelegate
 
@@ -17,7 +18,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    PaletteViewController *paletteViewController = [[PaletteViewController alloc] init];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:paletteViewController];
+    
+    self.window.rootViewController = navController;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ colordot-ios/PaletteViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaletteViewController.h
+//  colordot-ios
+//
+//  Created by Devin Hunt on 6/20/14.
+//  Copyright (c) 2014 Devin Hunt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaletteViewController : UIViewController
+
+@end
