668c06c0c2fb94123795d91151724c4221a0e5a5
Colin Jackson <colin.s.jackson90@gmail.com>
2014-08-09 02:11:47 -0400

    Add image editor-style “transparent” background to palette picker view

--- colordot-ios/PalettePickerViewController.m
+++ colordot-ios/PalettePickerViewController.m
@@ -37,7 +37,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"transparency"]];
 }
 
 - (void)didReceiveMemoryWarning
